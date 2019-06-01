021082bc89cf2be4b7d73ed0a245bb414e722fda
SamG <sam.gabrail@sysdig.com>
2019-05-31 20:59:22 -0400

    added Jenkins file for frontend

---
+++ create-cluster.sh
@@ -0,0 +1,1 @@
+gcloud beta container --project "vibrant-tree-219615" clusters create "sysdig-anthos-demo" --zone "us-central1-a" --no-enable-basic-auth --machine-type "n1-standard-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --num-nodes "3" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
